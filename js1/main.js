e86235bfa5f6522c7b6a54d5bc0d63aa00fa9eff
Ahyoung-Kim <eileel@naver.com>
2022-05-18 16:25:39 +0900

    클래스 이름 & css 선택자

--- js1/main.js
+++ js1/main.js
@@ -1,7 +1,11 @@
-const animals = document.getElementsByTagName("li")
+const animals = document.getElementsByClassName("animal")
 
 animals[0].style.color = 'red';
 animals[1].style.color = 'green';
 animals[2].style.color = 'blue';
+
+const animals2 = document.querySelectorAll(".animal")
+
+console.log(animals2)
 
 console.log(animals)
