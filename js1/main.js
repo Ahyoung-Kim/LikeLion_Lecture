60ca389fd81530cb56c52dc5f0c95555abb7e7f7
Ahyoung-Kim <eileel@naver.com>
2022-05-18 16:33:20 +0900

    innerHTML, classList.add, remove, toggle

--- js1/main.js
+++ js1/main.js
@@ -1,11 +1,17 @@
-const animals = document.getElementsByClassName("animal")
+const animal = document.getElementsByClassName("animal")
+const animals = document.querySelector("#animals")
 
-animals[0].style.color = 'red';
-animals[1].style.color = 'green';
-animals[2].style.color = 'blue';
+animal[0].style.color = 'red';
+animal[1].style.color = 'green';
+animal[2].style.color = 'blue';
 
-const animals2 = document.querySelectorAll(".animal")
+animal[0].innerText = "dog";
 
-console.log(animals2)
+animals.innerHTML += "<li>cat</li>"
 
-console.log(animals)
+const box = document.querySelector('.box')
+
+box.classList.add('purple')
+box.classList.remove('purple')
+
+box.classList.toggle('yellow')  // yellow 클래스가 있다면 제거하고, 없다면 추가 
